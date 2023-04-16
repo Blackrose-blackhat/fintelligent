915f1ec52823ec757ba32ef738e8ec7b6a3578a1
musharaf8651 <musharafz2k3@gmail.com>
2023-04-16 14:30:11 +0530

    home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "./home.css";
 import NavBar from "../../components/AppBar/AppBar";
-
+import { TypeAnimation } from "react-type-animation";
 function Home() {
   return (
     <div className="home">
       <div className="navbar">
         <NavBar />
+      </div>
+      <div className="home-bg">
+        <div className="bg-text">
+          <TypeAnimation
+            sequence={[
+              // Same String at the start will only be typed once, initially
+              "Stay Curious About Fianance",
+              1000,
+              "Stay Curious About Insaurance",
+              1000,
+              "Stay Curious About Credit Cards",
+              1000,
+              "Stay Curious About Domestic Market News",
+              1000,
+              "Stay Curious About Global Market News",
+              1000,
+              "Stay Curious About Investement and retirement plans",
+              1000,
+            ]}
+            speed={50}
+            cursor={false}
+            style={{ fontSize: "4rem", fontFamily: "serif" }}
+            repeat={Infinity}
+          />
+          <h3>Some quotes here</h3>
+        </div>
       </div>
     </div>
   );
