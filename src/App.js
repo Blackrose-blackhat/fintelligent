a2231676af5aab61636c59d01987c5eb0a1bc67c
musharaf8651 <musharafz2k3@gmail.com>
2023-06-03 00:43:11 +0530

    home pages added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import About from "./pages/About";
+import "./style.scss";
+import "./media-query.css";
 import NotFound from "./pages/NotFound";
 import AddEditblog from "./pages/AddEditblog";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { useState } from "react";
 function App() {
+  const [active, setActive] = useState("home");
+
   return (
     <div className="App">
+      <Header setActive={setActive} active={active} />
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
+import { Avatar } from "@mui/material";
 import React from "react";
-
-const Header = () => {
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.jpg";
+const Header = ({ setActive, active }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid bg-faded padding-media">
         <div className="container padding-media">
           <nav className="navbar navbar-toggleable-md navbar-light">
+            <div className="logo">
+              <Link
+                to="/"
+                style={{
+                  textDecoration: "none",
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <li
+                  onClick={() => setActive("home")}
+                  className={`nav-item nav-link ${
+                    active === "home" ? "active" : ""
+                  }`}
+                  style={{
+                    fontWeight: "bolder",
+                    letterSpacing: 2,
+                    fontSize: 22,
+                  }}
+                >
+                  FINTELLIGENT
+                </li>
+                <Avatar alt="Fintelligent" src={logo} />
+              </Link>
+            </div>
             <button
               className="navbar-toggler mt-3"
               type="button"
@@ -18,19 +45,46 @@ const Header = () => {
             >
               <span className="fa fa-bars"></span>
             </button>
+
             <div
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item nav-link">Home</li>
-                <li className="nav-item nav-link">Create</li>
-                <li className="nav-item nav-link">About</li>
+                <Link to="/create" style={{ textDecoration: "none" }}>
+                  <li
+                    onClick={() => setActive("create")}
+                    className={`nav-item nav-link ${
+                      active === "create" ? "active" : ""
+                    }`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    Create
+                  </li>
+                </Link>
+                <Link to="/about" style={{ textDecoration: "none" }}>
+                  <li
+                    onClick={() => setActive("about")}
+                    className={`nav-item nav-link ${
+                      active === "about" ? "active" : ""
+                    }`}
+                  >
+                    About
+                  </li>
+                </Link>
               </ul>
               <div className="row g-3">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item nav-link">Login</li>
-                  <li className="nav-item nav-link">Logout</li>
+                  <Link to="/auth" style={{ textDecoration: "none" }}>
+                    <li
+                      onClick={() => setActive("auth")}
+                      className={`nav-item nav-link ${
+                        active === "auth" ? "active" : ""
+                      }`}
+                    >
+                      Login
+                    </li>
+                  </Link>
                 </ul>
               </div>
             </div>
